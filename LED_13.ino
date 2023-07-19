922e0af78cf845146cd5b8fd325f1674c1266179
yeyoonn <139943442+yeyoonn@users.noreply.github.com>
2023-07-19 15:01:09 +0900

    Add files via upload

---
+++ LED_13.ino
@@ -0,0 +1,13 @@
+int led1=13;//led1은 13번 핀에 연결됨 led긴다리가 plus에 연결됨
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  pinMode(led1, OUTPUT);
+}
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(led1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(10);                       // wait for a second
+  digitalWrite(led1, LOW);    // turn the LED off by making the voltage LOW
+  delay(10);                       // wait for a second
+}
