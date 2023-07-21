dbd1006b53395216068dc69e18e66b4b88690a92
yeyoonn <139943442+yeyoonn@users.noreply.github.com>
2023-07-21 11:26:08 +0900

    Add files via upload

--- DHT11_LCD.ino
+++ DHT11_LCD.ino
@@ -1,7 +1,7 @@
 #include <SimpleDHT.h>
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x3F,16,2);  // set the LCD address to 0x3F for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x3F for a 16 chars and 2 line display
 // for DHT11, 
 //      VCC: 5V or 3V
 //      GND: GND
@@ -15,16 +15,16 @@ void setup() {
   // Print a message to the LCD.
   lcd.backlight();
   lcd.setCursor(3,0);
-  lcd.print("HELLO!");
+  lcd.print("Hello, world!");
   lcd.setCursor(2,1);
-  lcd.print("myeungmun!");
-  
+  lcd.print("Myeungmun !");
+
 }
 
 void loop() {
   lcd.init();
   // Print a message to the LCD.
-  lcd.backlight();  
+  //lcd.backlight();  
   // start working...
   Serial.println("=================================");
   Serial.println("Sample DHT11...");
@@ -42,19 +42,27 @@ void loop() {
   Serial.print("Sample OK: ");
   Serial.print((int)temperature); Serial.print(" *C, "); 
   Serial.print((int)humidity); Serial.println(" H");
-
+int t=temperature;
   lcd.init();
+  lcd.init();
+  lcd.clear();
   // Print a message to the LCD.
   lcd.backlight();
   lcd.setCursor(0,0);
   lcd.print("TEMP - ");
   lcd.setCursor(7,0);
-  lcd.println("temperature");
+  lcd.print(t);
+  lcd.setCursor(9,0);
+  lcd.write(223);
+  lcd.setCursor(10,0);
+  lcd.println("C");
+ lcd.setCursor(11,0);
+  lcd.println("    ");
   lcd.setCursor(0,1);
-  lcd.print("HUMI - ");
+  lcd.print("Humi = ");
   lcd.setCursor(7,1);
-  lcd.println("humidity");
-  lcd.setCursor(11,0);
+  lcd.println(humidity);
+  lcd.setCursor(11,1);
   lcd.print("%");
   // DHT11 sampling rate is 1HZ.
   delay(1500);
